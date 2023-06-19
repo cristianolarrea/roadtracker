bbf5dbf8a66cac433e6d17a24ec1e7a8dd55e001
Paloma <paloma.borges@fgv.edu.br>
2023-06-19 01:11:14 -0300

    dashboard

--- dash/app.py
+++ dash/app.py
@@ -1,12 +1,11 @@
 from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
-from dash_bootstrap_templates import ThemeSwitchAIO
 import plotly.express as px
 import pandas as pd
 
 # =====  Inicialização do Dash  ===== #
 app = Dash(__name__, 
-    external_stylesheets=[dbc.themes.CYBORG, "assets/style.css"], 
+    external_stylesheets=[dbc.themes.CYBORG, "assets/style.css", dbc.icons.FONT_AWESOME], 
     meta_tags=[{"charset": "utf-8"}, {"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 
 server = app.server
@@ -17,8 +16,8 @@ app.title = 'RoadTracker'
 app.layout = html.Div([
     dbc.Container([
         dbc.Row([
-            dbc.Col([
-                html.H1("Road Tracker Dashboard", className="title"),
+            dbc.Col([ 
+                html.H1([html.I(className="fa-solid fa-car"), " | Road Tracker Dashboard"], className="title"),
                 html.Div(className="line")
             ])
         ]),
@@ -27,19 +26,19 @@ app.layout = html.Div([
                 dbc.Row([
                     dbc.Col([
                         dbc.Card(
-                            [html.Div("6", className="card-text"),
+                            [html.Div("8", className="card-text"),
                             html.P("rodovias monitoradas")]
                         )
                     ], md=3, sm=6, xs=6),
                     dbc.Col([
                         dbc.Card(
-                            [html.Div("132", className="card-text"),
+                            [html.Div("276", className="card-text"),
                             html.P("veículos monitorados")]
                         )
                     ], md=3, sm=6, xs=6),
                     dbc.Col([
                         dbc.Card(
-                            [html.Div("10", className="card-text"),
+                            [html.Div("15", className="card-text"),
                             html.P("veículos acima da velocidade")]
                         )
                     ], md=3, sm=6, xs=6),
