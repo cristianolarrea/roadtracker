2d555bd792225d9b8f953a15b85daaeffd348454
cristiano.leal_ntconsult <junior.larrea.ids@gmail.com>
2023-06-20 22:44:42 -0300

    final local working

--- etl/analysis.py
+++ etl/analysis.py
@@ -42,7 +42,7 @@ while True:
     windowDept = Window.partitionBy("plate").orderBy(col("time").desc())
 
     df = df_original.withColumn("row",row_number().over(windowDept)) \
-            .filter(col("row") <= 3)
+        .filter(col("row") <= 3)
 
     # calculo da velocidade
     df = df.withColumn("speed", F.col("x") - F.lag("x", -1).over(windowDept))
@@ -53,60 +53,60 @@ while True:
     # drop null values
     df = df.na.drop()
     # -----------------------
-    
-    
+
+
     # -----------------------
     # DF DE RISCO DE COLISÃO
     windowDept = Window.partitionBy("road", "y").orderBy("x")
     # calcula o risco de colisao fazendo posicao + (velocidade * direcao) + (aceleracao * direcao)
     df = df.withColumn("collision_risk",
-                    F.when(F.col("direction") == 1,
-                            F.when((F.col("x") + F.col("speed") + F.col("acc")) > (F.lag("x", -1).over(windowDept) + F.lag("speed", -1).over(windowDept) + F.lag("acc",-1).over(windowDept)), 1).otherwise(0)) \
-                    .otherwise(F.when((F.col("x") - F.col("speed") - F.col("acc")) < (F.lag("x", 1).over(windowDept) - F.lag("speed", 1).over(windowDept) - F.lag("acc", 1).over(windowDept)), 1).otherwise(0)))
+                       F.when(F.col("direction") == 1,
+                              F.when((F.col("x") + F.col("speed") + F.col("acc")) > (F.lag("x", -1).over(windowDept) + F.lag("speed", -1).over(windowDept) + F.lag("acc",-1).over(windowDept)), 1).otherwise(0)) \
+                       .otherwise(F.when((F.col("x") - F.col("speed") - F.col("acc")) < (F.lag("x", 1).over(windowDept) - F.lag("speed", 1).over(windowDept) - F.lag("acc", 1).over(windowDept)), 1).otherwise(0)))
     # -----------------------
-    
-    
+
+
     # ----------------------- PRIORIDADE
     # ANALISE 6: LISTA DE VEICULOS COM RISCO DE COLISAO
     # Placa e velocidade
     CollisionRisk = df.filter(F.col("collision_risk") == 1) \
-                    .select("plate", "speed")
+        .select("plate", "speed")
 
     CollisionRisk.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "analysis6") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis6") \
+        .save()
     # -----------------------
 
 
     # -----------------------
     # ANALISE 1: NÚMERO DE RODOVIAS MONITORADAS
     n_roads = df_original.select("road").distinct().count()
-    
+
     # create a dataframe with the number of roads
     n_roads = spark.createDataFrame([(n_roads,)], ['n_roads'])
-    
+
     n_roads.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "analysis1") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis1") \
+        .save()
     # -----------------------
 
 
     # -----------------------
     # ANALISE 2: NUMERO TOTAL DE VEICULOS MONITORADOS
     n_cars = df_original.select("plate").distinct().count()
-    
+
     # create a dataframe with the number of cars
     n_cars = spark.createDataFrame([(n_cars,)], ['n_cars'])
-    
+
     n_cars.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "analysis2") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis2") \
+        .save()
     # -----------------------
 
 
@@ -119,15 +119,15 @@ while True:
         .select("plate") \
         .distinct() \
         .count()
-    
+
     # create a dataframe with the number of cars over the speed limit
     cars_over_speed_limit = spark.createDataFrame([(cars_over_speed_limit,)], ['cars_over_speed_limit'])
-        
+
     cars_over_speed_limit.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "analysis3") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis3") \
+        .save()
     # -----------------------
 
 
@@ -135,47 +135,30 @@ while True:
     # ANALISE 4: NUMERO DE VEICULOS COM RISCO DE COLISAO
     cars_collision_risk = df.filter(F.col("collision_risk") == 1) \
         .select("plate").distinct().count()
-        
+
     # create a dataframe with the number of cars with collision risk
     cars_collision_risk = spark.createDataFrame([(cars_collision_risk,)], ['cars_collision_risk'])
-        
+
     cars_collision_risk.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "analysis4") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis4") \
+        .save()
     # -----------------------
-    
+
 
     # -----------------------
     # ANALISE 5: LISTA DE VEICULOS ACIMA DO LIMITE DE VELOCIDADE
     # Placa, velocidade e se está com risco de colisão
-<<<<<<< HEAD
-    CollectionOverSpeedLimit = df.filter(F.col("over_speed_limit") == 1) \
-                            .select("plate", "speed", "collision_risk")
-
-
-    # ANALISE 6: LISTA DE VEICULOS COM RISCO DE COLISAO
-    # Placa e velocidade
-    CollectionCollisionRisk = df.filter(F.col("collision_risk") == 1) \
-                    .select("plate", "speed")
-
-    CollectionCollisionRisk.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "collisionRisk") \
-       .save()
-=======
     OverSpeedLimit = df.filter(F.col("over_speed_limit") == 1) \
-                .select("plate", "speed", "collision_risk")
-                            
+        .select("plate", "speed", "collision_risk")
+
     OverSpeedLimit.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "analysis5") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis5") \
+        .save()
     # -----------------------
->>>>>>> ce686b79134cbc74a81e660f20ce5edc2412838c
 
 
     # ############################################
@@ -188,12 +171,12 @@ while True:
 
     # get the top 100
     dfRoadCount = dfRoadCount.orderBy(col("road_count").desc()).limit(100)
-    
+
     dfRoadCount.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "historical1") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "historical1") \
+        .save()
     # --------------------
 
 
@@ -219,9 +202,9 @@ while True:
     # --------------------
     # ANALISE HISTORICA 2: ESTATISTICAS POR RODOVIA
     # get average speed per road
-    dfStats = dfCalcs.groupBy("road").avg("speed", "road_size")\
-                .withColumnRenamed("avg(speed)", "avg_speed")\
-                .withColumnRenamed("avg(road_size)", "road_size")
+    dfStats = dfCalcs.groupBy("road").avg("speed", "road_size") \
+        .withColumnRenamed("avg(speed)", "avg_speed") \
+        .withColumnRenamed("avg(road_size)", "road_size")
 
     # calculate avg time to cross
     dfStats = dfStats.withColumn("avg_time_to_cross", F.col( "road_size") / F.col("avg_speed")).drop("road_size")
@@ -234,15 +217,15 @@ while True:
 
     # join the dataframes to get all stats
     dfStats = dfStats.join(dfCollisions, "road", "left")
-    
+
     dfStats.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "historical2") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "historical2") \
+        .save()
     # --------------------
 
-    
+
     # --------------------
     # ANALISE HISTORICA 3: CARROS PROIBIDOS DE CIRCULAR POR DIREÇÃO PERIGOSA
     # partition by plate and order by time (twice to have ascending and descending row numbers)
@@ -255,13 +238,13 @@ while True:
 
     # check where speed is greater than 120 and the previous speed was less than road_speed (that is, new infraction)
     dfSpeeds = dfSpeeds.withColumn("change_in_speed",
-                    F.when(((F.col("speed") > F.col("road_speed")) & (F.lag("speed", -1).over(windowDept) <= F.lag("road_speed", -1).over(windowDept) )) , 1) \
-                    .otherwise(0))
+                                   F.when(((F.col("speed") > F.col("road_speed")) & (F.lag("speed", -1).over(windowDept) <= F.lag("road_speed", -1).over(windowDept) )) , 1) \
+                                   .otherwise(0))
 
     # check for vehicles that enter a road with speed > road_speed (infraction)
     dfSpeeds = dfSpeeds.withColumn("change_in_speed",
-                        F.when(((F.col("speed") > F.col("road_speed")) & (F.col("row2") ==1)), 1) \
-                            .otherwise(F.col("change_in_speed")))
+                                   F.when(((F.col("speed") > F.col("road_speed")) & (F.col("row2") ==1)), 1) \
+                                   .otherwise(F.col("change_in_speed")))
 
     # chosen T (change it after testing)
     t = 2500000000
@@ -273,13 +256,13 @@ while True:
 
     #  check which cars have more than 10 infractions
     dfInfractions = dfSpeeds.groupBy("plate").sum("change_in_speed") \
-    .withColumnRenamed("sum(change_in_speed)", "total_infractions").filter(F.col("total_infractions") >= 1)
-    
+        .withColumnRenamed("sum(change_in_speed)", "total_infractions").filter(F.col("total_infractions") >= 1)
+
     dfInfractions.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "historical3") \
-       .save()
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "historical3") \
+        .save()
     # --------------------
 
 
--- etl/analysisMongo.py
+++ etl/analysisMongo.py
@@ -32,22 +32,12 @@ while True:
 
     start_time = time.time()
 
-    # ANALISE 1: NÚMERO DE RODOVIAS MONITORADAS
-    n_roads = df_original.select("road").distinct().count()
-    #print("Number of roads: {}".format(n_roads))
-
-
-    # ANALISE 2: NUMERO TOTAL DE VEICULOS MONITORADOS
-    n_cars = df_original.select("plate").distinct().count()
-    #print("Number of cars: {}".format(n_cars))
-
-
     # -----------------------
     # VELOCIDADE E ACELERACAO
     windowDept = Window.partitionBy("plate").orderBy(col("time").desc())
 
     df = df_original.withColumn("row",row_number().over(windowDept)) \
-            .filter(col("row") <= 3)
+        .filter(col("row") <= 3)
 
     # calculo da velocidade
     df = df.withColumn("speed", F.col("x") - F.lag("x", -1).over(windowDept))
@@ -57,10 +47,65 @@ while True:
     df = df.withColumn("acc", F.col("speed") - F.lag("speed", -1).over(windowDept))
     # drop null values
     df = df.na.drop()
-    #df.show()
     # -----------------------
 
 
+    # -----------------------
+    # DF DE RISCO DE COLISÃO
+    windowDept = Window.partitionBy("road", "y").orderBy("x")
+    # calcula o risco de colisao fazendo posicao + (velocidade * direcao) + (aceleracao * direcao)
+    df = df.withColumn("collision_risk",
+                       F.when(F.col("direction") == 1,
+                              F.when((F.col("x") + F.col("speed") + F.col("acc")) > (F.lag("x", -1).over(windowDept) + F.lag("speed", -1).over(windowDept) + F.lag("acc",-1).over(windowDept)), 1).otherwise(0)) \
+                       .otherwise(F.when((F.col("x") - F.col("speed") - F.col("acc")) < (F.lag("x", 1).over(windowDept) - F.lag("speed", 1).over(windowDept) - F.lag("acc", 1).over(windowDept)), 1).otherwise(0)))
+    # -----------------------
+
+
+    # ----------------------- PRIORIDADE
+    # ANALISE 6: LISTA DE VEICULOS COM RISCO DE COLISAO
+    # Placa e velocidade
+    CollisionRisk = df.filter(F.col("collision_risk") == 1) \
+        .select("plate", "speed")
+
+    CollisionRisk.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis6") \
+        .save()
+    # -----------------------
+
+
+    # -----------------------
+    # ANALISE 1: NÚMERO DE RODOVIAS MONITORADAS
+    n_roads = df_original.select("road").distinct().count()
+
+    # create a dataframe with the number of roads
+    n_roads = spark.createDataFrame([(n_roads,)], ['n_roads'])
+
+    n_roads.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis1") \
+        .save()
+    # -----------------------
+
+
+    # -----------------------
+    # ANALISE 2: NUMERO TOTAL DE VEICULOS MONITORADOS
+    n_cars = df_original.select("plate").distinct().count()
+
+    # create a dataframe with the number of cars
+    n_cars = spark.createDataFrame([(n_cars,)], ['n_cars'])
+
+    n_cars.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis2") \
+        .save()
+    # -----------------------
+
+
+    # -----------------------
     # ANALISE 3: NUMERO DE VEICULOS ACIMA DO LIMITE DE VELOCIDADE
     # add a column for the cars over the speed limit
     df = df.withColumn("over_speed_limit", F.when(F.col("speed") > F.col("road_speed"), 1).otherwise(0))
@@ -69,59 +114,71 @@ while True:
         .select("plate") \
         .distinct() \
         .count()
-    #print("Number of cars over the speed limit: {}".format(cars_over_speed_limit))
 
+    # create a dataframe with the number of cars over the speed limit
+    cars_over_speed_limit = spark.createDataFrame([(cars_over_speed_limit,)], ['cars_over_speed_limit'])
 
-    # -----------------------
-    # DF DE RISCO DE COLISÃO
-    windowDept = Window.partitionBy("road", "y").orderBy("x")
-    # calcula o risco de colisao fazendo posicao + (velocidade * direcao) + (aceleracao * direcao)
-    df = df.withColumn("collision_risk",
-                    F.when(F.col("direction") == 1,
-                            F.when((F.col("x") + F.col("speed") + F.col("acc")) > (F.lag("x", -1).over(windowDept) + F.lag("speed", -1).over(windowDept) + F.lag("acc",-1).over(windowDept)), 1).otherwise(0)) \
-                    .otherwise(F.when((F.col("x") - F.col("speed") - F.col("acc")) < (F.lag("x", 1).over(windowDept) - F.lag("speed", 1).over(windowDept) - F.lag("acc", 1).over(windowDept)), 1).otherwise(0)))
+    cars_over_speed_limit.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis3") \
+        .save()
     # -----------------------
 
 
+    # -----------------------
     # ANALISE 4: NUMERO DE VEICULOS COM RISCO DE COLISAO
     cars_collision_risk = df.filter(F.col("collision_risk") == 1) \
         .select("plate").distinct().count()
 
+    # create a dataframe with the number of cars with collision risk
+    cars_collision_risk = spark.createDataFrame([(cars_collision_risk,)], ['cars_collision_risk'])
 
+    cars_collision_risk.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis4") \
+        .save()
+    # -----------------------
+
+
+    # -----------------------
     # ANALISE 5: LISTA DE VEICULOS ACIMA DO LIMITE DE VELOCIDADE
     # Placa, velocidade e se está com risco de colisão
-    CollectionOverSpeedLimit = df.filter(F.col("over_speed_limit") == 1) \
-                            .select("plate", "speed", "collision_risk")
+    OverSpeedLimit = df.filter(F.col("over_speed_limit") == 1) \
+        .select("plate", "speed", "collision_risk")
 
-
-    # ANALISE 6: LISTA DE VEICULOS COM RISCO DE COLISAO
-    # Placa e velocidade
-    CollectionCollisionRisk = df.filter(F.col("collision_risk") == 1) \
-                    .select("plate", "speed")
-
-    CollectionCollisionRisk.write.format("mongodb") \
-       .mode("overwrite") \
-       .option("database", "roadtracker") \
-       .option("collection", "collisionRisk") \
-       .save()
+    OverSpeedLimit.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "analysis5") \
+        .save()
+    # -----------------------
 
 
     # ############################################
     # ---------------- HISTORICAS ----------------
     # ############################################
 
+    # --------------------
     # ANALISE HISTORICA 1: TOP 100 VEICULOS QUE PASSARAM POR MAIS RODOVIAS
     dfRoadCount = df_original.groupBy("plate").agg(countDistinct('road')).withColumnRenamed("count(road)", "road_count")
 
     # get the top 100
     dfRoadCount = dfRoadCount.orderBy(col("road_count").desc()).limit(100)
-    #dfRoadCount.show()
+
+    dfRoadCount.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "historical1") \
+        .save()
+    # --------------------
 
 
     # --------------------
     # CALCULO TODAS AS VELOCIDADES
     windowDept = Window.partitionBy("plate").orderBy(col("time").desc())
-    dfCalcs = df.withColumn("row",row_number().over(windowDept))
+    dfCalcs = df_original.withColumn("row",row_number().over(windowDept))
 
     # calc all speeds
     dfCalcs = dfCalcs.withColumn("speed", F.col("x") - F.lag("x", -1).over(windowDept))
@@ -137,12 +194,12 @@ while True:
     # --------------------
 
 
-
+    # --------------------
     # ANALISE HISTORICA 2: ESTATISTICAS POR RODOVIA
     # get average speed per road
-    dfStats = dfCalcs.groupBy("road").avg("speed", "road_size")\
-                .withColumnRenamed("avg(speed)", "avg_speed")\
-                .withColumnRenamed("avg(road_size)", "road_size")
+    dfStats = dfCalcs.groupBy("road").avg("speed", "road_size") \
+        .withColumnRenamed("avg(speed)", "avg_speed") \
+        .withColumnRenamed("avg(road_size)", "road_size")
 
     # calculate avg time to cross
     dfStats = dfStats.withColumn("avg_time_to_cross", F.col( "road_size") / F.col("avg_speed")).drop("road_size")
@@ -156,8 +213,15 @@ while True:
     # join the dataframes to get all stats
     dfStats = dfStats.join(dfCollisions, "road", "left")
 
+    dfStats.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "historical2") \
+        .save()
+    # --------------------
 
 
+    # --------------------
     # ANALISE HISTORICA 3: CARROS PROIBIDOS DE CIRCULAR POR DIREÇÃO PERIGOSA
     # partition by plate and order by time (twice to have ascending and descending row numbers)
     windowDept = Window.partitionBy("plate").orderBy(col("time").desc())
@@ -169,13 +233,13 @@ while True:
 
     # check where speed is greater than 120 and the previous speed was less than road_speed (that is, new infraction)
     dfSpeeds = dfSpeeds.withColumn("change_in_speed",
-                    F.when(((F.col("speed") > F.col("road_speed")) & (F.lag("speed", -1).over(windowDept) <= F.lag("road_speed", -1).over(windowDept) )) , 1) \
-                    .otherwise(0))
+                                   F.when(((F.col("speed") > F.col("road_speed")) & (F.lag("speed", -1).over(windowDept) <= F.lag("road_speed", -1).over(windowDept) )) , 1) \
+                                   .otherwise(0))
 
     # check for vehicles that enter a road with speed > road_speed (infraction)
     dfSpeeds = dfSpeeds.withColumn("change_in_speed",
-                        F.when(((F.col("speed") > F.col("road_speed")) & (F.col("row2") ==1)), 1) \
-                            .otherwise(F.col("change_in_speed")))
+                                   F.when(((F.col("speed") > F.col("road_speed")) & (F.col("row2") ==1)), 1) \
+                                   .otherwise(F.col("change_in_speed")))
 
     # chosen T (change it after testing)
     t = 2500000000
@@ -187,8 +251,14 @@ while True:
 
     #  check which cars have more than 10 infractions
     dfInfractions = dfSpeeds.groupBy("plate").sum("change_in_speed") \
-    .withColumnRenamed("sum(change_in_speed)", "total_infractions").filter(F.col("total_infractions") >= 10)
+        .withColumnRenamed("sum(change_in_speed)", "total_infractions").filter(F.col("total_infractions") >= 1)
 
+    dfInfractions.write.format("mongodb") \
+        .mode("overwrite") \
+        .option("database", "roadtracker") \
+        .option("collection", "historical3") \
+        .save()
+    # --------------------
 
 
     print("--- %s seconds ---" % (time.time() - start_time))
